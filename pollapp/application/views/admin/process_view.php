007fccbf14c82748d909434599c546695ac80cc2
Leo <aito0077@gmail.com>
2013-02-15 13:37:07 -0300

    Move project to surveyapp.

--- pollapp/application/views/admin/process_view.php
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE html>
-<div metal:use-macro="./wrapper/process_wrapper.html/process_wrapper">
-    <div metal:fill-slot="main_process_view">
-        Esto es una prueba
-    </div>
-</div>
--- pollapp/static/js/commons/time_tracking.js
+++
@@ -1,8 +0,0 @@
-function start_tracking() { 
-    this.start_time = new Date();
-}
-
-function stop_tracking() {
-    return (new Date() - this.start_time);
-}
-
